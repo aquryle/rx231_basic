321f45ec968b885eb5f6933f230207ae81f94fb7
aquryle <aquryle@gmail.com>
2024-06-01 22:20:44 +0900

    first commit

---
+++ Release/generate/subdir.mk
@@ -0,0 +1,50 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LINKER_SCRIPT += \
+../generate/linker_script.ld 
+
+S_UPPER_SRCS += \
+../generate/start.S 
+
+C_SRCS += \
+../generate/hwinit.c \
+../generate/inthandler.c \
+../generate/vects.c 
+
+LST += \
+hwinit.lst \
+inthandler.lst \
+start.lst \
+vects.lst 
+
+C_DEPS += \
+./generate/hwinit.d \
+./generate/inthandler.d \
+./generate/vects.d 
+
+OBJS += \
+./generate/hwinit.o \
+./generate/inthandler.o \
+./generate/start.o \
+./generate/vects.o 
+
+MAP += \
+rx231_basic.map 
+
+S_UPPER_DEPS += \
+./generate/start.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+generate/%.o: ../generate/%.c
+	@echo 'Building file: $<'
+	$(file > $@.in,-Og -ffunction-sections -fdata-sections -fno-strict-aliasing -fdiagnostics-parseable-fixits -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Waggregate-return -Wstack-usage=100 -g2 -misa=v2 -mlittle-endian-data -I"C:/Users/sata/develop/e2studio_2024_01/RX231/rx231_basic/generate" -Wa,-adlnh="$(basename $(notdir $<)).lst" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" "$<" -c -o "$@")
+	@rx-elf-gcc @"$@.in"
+generate/%.o: ../generate/%.S
+	@echo 'Building file: $<'
+	$(file > $@.in,-Og -ffunction-sections -fdata-sections -fno-strict-aliasing -fdiagnostics-parseable-fixits -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Waggregate-return -Wstack-usage=100 -g2 -misa=v2 -mlittle-endian-data -x assembler-with-cpp -Wa,--gdwarf2 -I"C:/Users/sata/develop/e2studio_2024_01/RX231/rx231_basic/src" -Wa,-adlhn="$(basename $(notdir $<)).lst" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c "$<" -o "$@")
+	@rx-elf-gcc @"$@.in"
+
---
+++ Release/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/rx231_basic.c 
+
+LST += \
+rx231_basic.lst 
+
+C_DEPS += \
+./src/rx231_basic.d 
+
+OBJS += \
+./src/rx231_basic.o 
+
+MAP += \
+rx231_basic.map 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	$(file > $@.in,-Og -ffunction-sections -fdata-sections -fno-strict-aliasing -fdiagnostics-parseable-fixits -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Waggregate-return -Wstack-usage=100 -g2 -misa=v2 -mlittle-endian-data -I"C:/Users/sata/develop/e2studio_2024_01/RX231/rx231_basic/generate" -Wa,-adlnh="$(basename $(notdir $<)).lst" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" "$<" -c -o "$@")
+	@rx-elf-gcc @"$@.in"
+
